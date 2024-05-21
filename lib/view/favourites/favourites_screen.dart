b07404a0c8e52a363d616e5f33e056186c1be8eb
Babar78 <muhammadbabarashraf8@gmail.com>
2024-05-21 07:13:25 +0500

    refactor: Remove unused imports and widgets in FavouritesScreen

--- lib/view/favourites/favourites_screen.dart
+++ lib/view/favourites/favourites_screen.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:sole_quest/animation/fadeanimation.dart';
 import 'package:sole_quest/data/dummy_data.dart';
 import 'package:sole_quest/models/models.dart';
-import 'package:sole_quest/theme/custom_app_theme.dart';
-import 'package:sole_quest/utils/app_methods.dart';
 import 'package:sole_quest/utils/constants.dart';
 import 'package:sole_quest/view/favourites/widget/app_bar.dart';
 import 'package:sole_quest/view/favourites/widget/body.dart';
-import 'package:sole_quest/view/view.dart';
 import 'package:sole_quest/widget/snack_bar.dart';
 
 class FavouritesScreen extends StatefulWidget {
